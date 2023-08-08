2fcd2c6ed4400c0baf215cad5e110a438d0aa8da
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-08-08 23:29:58 +0300

    Add files via upload

---
+++ Arrays Exercises/1. Write a Program to Store User Input in an Array/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+namespace MyProject;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Please type in what size of an array you want to create!");
+        int numberOfInputs = int.Parse(Console.ReadLine());
+        string[] array = new string[numberOfInputs];
+
+
+        Console.WriteLine($"Now type in {numberOfInputs} times in the created array!");
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = Console.ReadLine();
+        }
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine($"array[{i}] = {array[i]}");
+        }
+    }
+}
