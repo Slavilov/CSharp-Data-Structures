aaf500bb3cb107e60532b6757dd51a1d578da71e
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-08-10 11:13:38 +0300

    Add files via upload

---
+++ Arrays Exercises/3. Find the Sum of All Array Elements/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+namespace MyProject;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Please type in what size of an array you want to create!");
+        int numberOfInputs = int.Parse(Console.ReadLine());
+        int[] array = new int[numberOfInputs];
+        int sumOfAllElements = 0;
+
+
+        Console.WriteLine($"Now type in {numberOfInputs} numbers, WHOLE NUMBERS (int) in the created array!");
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = int.Parse(Console.ReadLine());
+        }
+
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            sumOfAllElements += array[i];
+        }
+
+        Console.WriteLine($"The largest number in the array is {sumOfAllElements}");
+
+
+    }
+}
