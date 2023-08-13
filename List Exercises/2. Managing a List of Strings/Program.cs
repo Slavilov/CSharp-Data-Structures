2c1c4cc1e78a9c9a3a393ac908a04bab03c2e260
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-08-13 22:40:58 +0300

    Add files via upload

---
+++ List Exercises/2. Managing a List of Strings/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BasicListOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+
+            while (true) 
+            {
+                string nameToAdd = Console.ReadLine();
+                if (nameToAdd != "stop")
+                {
+                    names.Add(nameToAdd);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            foreach (var name in names)
+            {
+                if (name.Contains('a'))
+                {
+                    Console.WriteLine($"{name}");
+                }
+            }
+        }
+    }
+}
