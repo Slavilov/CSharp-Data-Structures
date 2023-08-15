a5bfa594bcd3901c51293af2da9350d895c0d681
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-08-15 14:51:46 +0300

    Add files via upload

---
+++ List Exercises/4. Implementing a Stack Using a List/Program.cs
@@ -0,0 +1,49 @@
+﻿using _4._Implementing_a_Stack_Using_a_List;
+using System;
+using System.Collections.Generic;
+
+namespace BasicListOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack customStack = new Stack();
+
+            Console.WriteLine("Now add as many elements as you want, until you say 'stop'!");
+
+            while(true)
+            {
+                string element = Console.ReadLine();
+                if (element == "stop")
+                {
+                    break;
+                }
+                else
+                {
+                    customStack.Push(element);
+                }
+            }
+
+            Console.WriteLine("The following elements are in the stack:");
+            foreach (var item in customStack.List)
+            {
+                Console.WriteLine($"{item}");
+            }
+
+            Console.WriteLine("How many items do you want to 'Pop' out of the stack? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                customStack.Pop();
+            }
+
+            Console.WriteLine("The following elements are left in the stack:");
+            foreach (var item in customStack.List)
+            {
+                Console.WriteLine($"{item}");
+            }
+        }
+    }
+}
---
+++ List Exercises/4. Implementing a Stack Using a List/Stack.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._Implementing_a_Stack_Using_a_List
+{
+    internal class Stack
+    {
+        public Stack()
+        {
+            List = new List<string>();
+        }
+        private List<string> list;
+
+		public List<string> List
+		{
+			get { return list; }
+			set { list = value; }
+		}
+
+        public void Push(string item)
+        {
+            list.Add(item);
+        }
+
+        public string Pop()
+        {
+            int count = list.Count;
+            if (count >0) 
+            {
+                string poppedElement = list[count - 1];
+                list.RemoveAt(count-1);
+                return poppedElement;
+            }
+            else
+            {
+                throw InvalidOperationException();
+            }
+        }
+
+        public string Peek()
+        {
+            int count = list.Count;
+
+            if (count > 0)
+            {
+                string peekedElement = list[count - 1];
+                return peekedElement;
+            }
+            else
+            {
+                throw InvalidOperationException();
+            }
+
+        }
+
+        private Exception InvalidOperationException()
+        {
+            throw new NotImplementedException();
+        }
+
+	}
+}
