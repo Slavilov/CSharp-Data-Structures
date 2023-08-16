f7cd5c620c2367e63b995ec41530a37537712d47
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-08-16 23:41:01 +0300

    Add files via upload

---
+++ List Exercises/5. Removing Duplicates/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BasicListOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Now i want you to type with spaces in between every element of a list! ");
+            List<string> strings = Console.ReadLine().Split(' ').ToList();
+
+            Console.WriteLine("These are the strings inside the list: ");
+            foreach (var item in strings)
+            {
+                Console.WriteLine($"{item}");
+            }
+
+            for (int i = 0; i < strings.Count; i++)
+            {
+                for (int j = 0; j < strings.Count; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+
+                    if (strings[i] == strings[j])
+                    {
+                        strings.RemoveAll(x => x == strings[j]);
+                    }
+                }
+            }
+
+            Console.WriteLine("These are the strings inside the list after removing the duplicates: ");
+            foreach (var item in strings)
+            {
+                Console.WriteLine($"{item}");
+            }
+        }
+    }
+}
