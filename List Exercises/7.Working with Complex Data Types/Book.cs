6aef30d4e1067975eec1c9c6d26f847e456c9cc7
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-08-17 23:10:04 +0300

    Add files via upload

---
+++ List Exercises/7.Working with Complex Data Types/Book.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.Working_with_Complex_Data_Types
+{
+    internal class Book
+    {
+		private double rating;
+		private string author;
+		private string tittle;
+
+        public Book(string tittle, string author, double rating)
+        {
+            this.Tittle = tittle;
+			this.Author = author;
+			this.Rating = rating;
+        }
+
+        public double Rating
+		{
+			get { return rating; }
+			private set { rating = value; }
+		}
+
+		public string Author
+		{
+			get { return author; }
+            private set { author = value; }
+		}
+
+		public string Tittle
+		{
+			get { return tittle; }
+            private set { tittle = value; }
+		}
+
+	}
+}
---
+++ List Exercises/7.Working with Complex Data Types/Program.cs
@@ -0,0 +1,56 @@
+﻿using _7.Working_with_Complex_Data_Types;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BasicListOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book> books = new List<Book>();
+
+            Console.WriteLine("Now type in the Tittle, Author and Rating of the book with spaces in between them and th rating should be a double number example 5.00! say 'Stop' to stop!");
+            while (true)
+            {
+                List<string> strings = Console.ReadLine().Split(' ').ToList();
+
+                if (strings[0] == "Stop")
+                {
+                    break;
+                }
+
+                Book newBook = new Book(strings[0], strings[1], double.Parse(strings[2]));
+                books.Add(newBook);
+            }
+
+            Console.WriteLine("These are the books added in the list: ");
+            foreach (var book in books)
+            {
+                Console.WriteLine($"Book name: {book.Tittle} Author: {book.Author} Rating: {book.Rating}");
+            }
+        }
+        public static void PrintBooksByAuthor(List<Book> books, string author)
+        {
+            foreach (var book in books)
+            {
+                if (book.Author == author)
+                {
+                    Console.WriteLine($"Title: {book.Tittle}, Author: {book.Author}");
+                }
+            }
+        }
+
+        public static void PrintBooksWithRatingAbove(List<Book> books, double rating)
+        {
+            foreach (var book in books)
+            {
+                if (book.Rating >= rating)
+                {
+                    Console.WriteLine($"Title: {book.Tittle}, Author: {book.Author}");
+                }
+            }
+        }
+    }
+}
