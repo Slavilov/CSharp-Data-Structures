0a9dbd3ef33314e4867ef7d99cf0e8fb606e045d
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-08-23 23:13:18 +0300

    Add files via upload

---
+++ Dictionary Exercises/2. Word Frequency Counter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Channels;
+
+namespace MethodsExercise;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please type in a sentence, a paragraph or a text");
+        string text = Console.ReadLine();
+        Dictionary<string, int> wordFrequency = new Dictionary<string, int>();
+        string[] words = text.Split(new char[] { ',', ' ', '|' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+        
+        foreach (string word in words)
+        {
+            if (wordFrequency.ContainsKey(word))
+            {
+                wordFrequency[word]++;
+            }
+            else
+            {
+                wordFrequency.Add(word, 1);
+            }
+        }
+
+        foreach (var word in wordFrequency)
+        {
+            Console.WriteLine($"Word: {word.Key} appears {word.Value} times in the text!");
+        }
+    }
+}
