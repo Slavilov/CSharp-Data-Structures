b2689f42fe18a260226510febd067ea3709c5a2b
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-09-02 12:33:22 +0300

    Add files via upload

---
+++ Stack Exercises/2. Check Balanced Parentheses - Stack/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string expression = "{[()]}";
+        Stack<char> stack = new Stack<char>();
+        bool isBalanced = true;
+
+        foreach (char ch in expression)
+        {
+            if (ch == '{' || ch == '[' || ch == '(')
+            {
+                stack.Push(ch);
+            }
+            else if (ch == '}' || ch == ']' || ch == ')')
+            {
+                if (stack.Count == 0)
+                {
+                    isBalanced = false;
+                    break;
+                }
+
+                char last = stack.Pop();
+                if (!((last == '{' && ch == '}') || (last == '[' && ch == ']') || (last == '(' && ch == ')')))
+                {
+                    isBalanced = false;
+                    break;
+                }
+            }
+        }
+
+        if (stack.Count != 0)
+        {
+            isBalanced = false;
+        }
+
+        Console.WriteLine(isBalanced ? "Balanced" : "Not Balanced");
+    }
+}
