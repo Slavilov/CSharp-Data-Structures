f30fc06adb165daa3cbdc05c0f348034cf0ef277
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-09-05 23:57:53 +0300

    Add files via upload

---
+++ Queue Exercises/3. Generate Binary Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Queue<string> queue = new Queue<string>();
+        int n = 10;  // Number of binary numbers you want to generate
+        queue.Enqueue("1");
+
+        while (n-- > 0)
+        {
+            string s1 = queue.Dequeue();
+            Console.WriteLine(s1);
+
+            string s2 = s1;
+
+            queue.Enqueue(s1 + "0");
+            queue.Enqueue(s2 + "1");
+        }
+    }
+}
